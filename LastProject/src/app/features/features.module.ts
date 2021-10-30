b900970309f10f8f22e2d12931b70b27094f99a2
Hanya Adel <haniajed79@gmail.com>
2021-10-30 21:21:58 +0200

    task feature

--- LastProject/src/app/features/features.module.ts
+++ LastProject/src/app/features/features.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrashTaskComponent } from './trash-task/trash-task.component';
+import { TasksMainComponent } from './tasks-main/tasks-main.component';
 
 
 
 
 @NgModule({
   declarations: [
-    TrashTaskComponent
+    TrashTaskComponent,
+    TasksMainComponent
   ],
   imports: [
     CommonModule,
     
   ],
-  exports:[TrashTaskComponent]
+  exports:[TrashTaskComponent,
+    TasksMainComponent
+  ]
 })
 export class FeaturesModule { }
---
+++ LastProject/src/app/features/tasks-main/tasks-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-main',
+  templateUrl: './tasks-main.component.html',
+  styleUrls: ['./tasks-main.component.css']
+})
+export class TasksMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
